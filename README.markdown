a03f10af7fe90edc77cb0281d20c25d616bc697c
David Logie <d@djl.im>
2011-10-06 15:08:01 +0100

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,4 @@
+findflip
+--------
+
+Toggle the find bar.
---
+++ src/chrome/content/findflip.js
@@ -0,0 +1,8 @@
+function findflip() {
+    var findbar = document.getElementById("FindToolbar");
+	if (findbar == null || findbar.hidden) {
+		gFindBar.onFindCommand();
+	} else {
+		findbar.close();
+	}
+}
