47b37fd56f3ea2cfd7819286f6d8c476db078da6
David Logie <d@djl.im>
2012-03-16 07:55:31 +0000

    Slight cleanup.

--- src/chrome/content/findflip.js
+++ src/chrome/content/findflip.js
@@ -2,10 +2,8 @@ function findflip(openit) {
     var findbar = document.getElementById("FindToolbar");
     if (openit == null && (findbar == null || findbar.hidden)) {
         gFindBar.onFindCommand();
-    } else {
-        if (findbar) {
-            findbar.close();
-        }
+    } else if (findbar) {
+        findbar.close();
     }
 }
 
