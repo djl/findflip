28021e52719493f6116287bdb466a33cfd887e65
David Logie <d@djl.im>
2014-01-29 19:34:27 +0000

    Make this work again.

--- src/chrome/content/findflip.js
+++ src/chrome/content/findflip.js
@@ -1,14 +1,7 @@
 function findflip(openit) {
-    var findbar = document.getElementById('FindToolbar');
-    if (openit == null && (findbar == null || findbar.hidden)) {
+    if (gFindBar.hidden) {
         gFindBar.onFindCommand();
-    } else if (findbar) {
-        findbar.close();
+    } else {
+        gFindBar.close();
     }
 }
-
-window.addEventListener('click', function(event) {
-    if (event.target.nodeName != 'findbar') {
-        findflip('click');
-    }
-}, false);
